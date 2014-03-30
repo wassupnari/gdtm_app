eb834817bfe05981dd64934f47776f25f8e614dc
narikim <wassupnari@gmail.com>
2014-03-29 17:25:38 -0700

    [Main] Add progressBar on Main page

--- GDTMRole/src/com/gdtm/app/fragment/FragmentMain.java
+++ GDTMRole/src/com/gdtm/app/fragment/FragmentMain.java
@@ -14,13 +14,14 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ExpandableListView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 
 /**
  * This is the main page of role signup.
  * 
  * @author Nari Kim Shin (wassupnari@gmail.com)
  */
-public class FragmentMain extends Fragment {
+public class FragmentMain extends Fragment implements Runnable {
 
 	private static final int NUM_OF_ROLE = 18;
 
@@ -28,15 +29,37 @@ public class FragmentMain extends Fragment {
 
 	private ArrayList<String> mGroupItem = new ArrayList<String>();
 	private ArrayList<Object> mChildItem = new ArrayList<Object>();
+	
+	private ProgressBar CCProgressBar;
+	private ProgressBar CLProgressBar;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-		View view = inflater.inflate(R.layout.fragment_upcoming_meeting, null);
-
+		View view = inflater.inflate(R.layout.fragment_main, null);
+		
+		CCProgressBar = (ProgressBar) view.findViewById(R.id.cc_progress);
+		CCProgressBar.setVisibility(ProgressBar.VISIBLE);
+        CCProgressBar.setProgress(0);
+        CCProgressBar.setMax(50);
+		new Thread(this).start();
 
 		return view;
 	}
 
-	
+	@Override
+    public void run() {
+        int currentPosition= 0;
+        while (currentPosition<100) {
+            try {
+                Thread.sleep(100);
+                currentPosition+= 1;
+            } catch (InterruptedException e) {
+                return;
+            } catch (Exception e) {
+                return;
+            }            
+            CCProgressBar.setProgress(currentPosition);
+        }
+    }
 }
