e49a2a83af9c6bb3eb92e312c50c8198728239dc
narikim <wassupnari@gmail.com>
2014-04-20 19:18:57 -0700

    [CL] Role description dialog added

--- GDTMRole/src/com/nari/somnium/activity/CLDetailActivity.java
+++ GDTMRole/src/com/nari/somnium/activity/CLDetailActivity.java
@@ -1,17 +1,22 @@
 package com.nari.somnium.activity;
 
 import com.gdtm.app.R;
+import com.nari.somnium.adapter.CLManualDialogAdapter;
 import com.nari.somnium.helper.DatabaseHelper;
 import com.nari.somnium.pojo.CLDataPojo;
 import com.nari.somnium.pojo.CLSubDataPojo;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
@@ -20,27 +25,27 @@ import android.widget.ToggleButton;
  */
 
 public class CLDetailActivity extends BaseActivity {
-	
+
 	private TextView mEvaluatorStatic;
 	private TextView mEvaluator;
 	private EditText mEvaluatorEdit;
-	
+
 	private TextView mDateStatic;
 	private TextView mDate;
 	private EditText mDateEdit;
-	
+
 	private TextView mCommentStatic;
 	private TextView mComment;
 	private EditText mCommentEdit;
-	
+
 	private int mProjectNumber;
 	private int mSubNumber;
-	
+
 	private ToggleButton mComplete;
-	
+
 	private CLDataPojo mDataList;
 	private CLSubDataPojo mData;
-	
+
 	private DatabaseHelper mDB;
 
 	@Override
@@ -51,47 +56,47 @@ public class CLDetailActivity extends BaseActivity {
 		mProjectNumber = getIntent().getIntExtra("cl_group_id", 0);
 		mSubNumber = getIntent().getIntExtra("cl_child_id", 0);
 		showActionBar(this, title);
-		
+
 		mDB = new DatabaseHelper(this);
-		
+
 		mDataList = mDB.getUserCLData(mProjectNumber);
 		mDataList.setId(mProjectNumber);
 		mData = mDataList.getSubData().get(mSubNumber);
-		
+
 		setupUI();
-		
+
 	}
-	
+
 	public void setupUI() {
 		mEvaluatorStatic = (TextView) findViewById(R.id.cl_evaluator_static);
 		mEvaluator = (TextView) findViewById(R.id.cl_evaluator);
 		mEvaluatorEdit = (EditText) findViewById(R.id.cl_evaluator_edit);
-		
+
 		mDateStatic = (TextView) findViewById(R.id.cl_date_static);
 		mDate = (TextView) findViewById(R.id.cl_date);
 		mDateEdit = (EditText) findViewById(R.id.cl_date_edit);
-		
+
 		mCommentStatic = (TextView) findViewById(R.id.cl_comment_static);
 		mComment = (TextView) findViewById(R.id.cl_comment);
 		mCommentEdit = (EditText) findViewById(R.id.cl_comment_edit);
-		
+
 		mEvaluatorStatic.setText("Evaluator : ");
 		mDateStatic.setText("Date : ");
 		mCommentStatic.setText("Comment : ");
-		
-		if(mData != null) {
+
+		if (mData != null) {
 			mEvaluator.setText(mData.getEvaluator());
 			mDate.setText(mData.getDate());
 			mComment.setText(mData.getComment());
 		}
-		
+
 		mComplete = (ToggleButton) findViewById(R.id.footer_toggle_cl);
-		if(mData.getComplete()) {
+		if (mData.getComplete()) {
 			mComplete.setChecked(true);
 		} else {
 			mComplete.setChecked(false);
 		}
-		
+
 		mComplete.setOnCheckedChangeListener(new ToggleButton.OnCheckedChangeListener() {
 
 			@Override
@@ -100,11 +105,9 @@ public class CLDetailActivity extends BaseActivity {
 				mDataList.getSubData().set(mSubNumber, mData);
 				mDB.updateCL(mDataList);
 			}
-			
+
 		});
 	}
-	
-	
 
 	@Override
 	protected void onDestroy() {
@@ -119,7 +122,7 @@ public class CLDetailActivity extends BaseActivity {
 		mEvaluator.setVisibility(View.GONE);
 		mDate.setVisibility(View.GONE);
 		mComment.setVisibility(View.GONE);
-		
+
 		mEvaluatorEdit.setVisibility(View.VISIBLE);
 		mEvaluatorEdit.setText(mData.getEvaluator());
 		mDateEdit.setVisibility(View.VISIBLE);
@@ -131,39 +134,61 @@ public class CLDetailActivity extends BaseActivity {
 	@Override
 	public void onEditDone() {
 		super.onEditDone();
-		
+
 		// Hide keyboard
 		InputMethodManager inputManager = (InputMethodManager) getSystemService(getApplicationContext().INPUT_METHOD_SERVICE);
 		if (inputManager.isAcceptingText()) {
-			inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+			inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+					InputMethodManager.HIDE_NOT_ALWAYS);
 		}
-		
+
 		mEvaluator.setVisibility(View.VISIBLE);
 		mDate.setVisibility(View.VISIBLE);
 		mComment.setVisibility(View.VISIBLE);
-		
+
 		String evaluator = mEvaluatorEdit.getText().toString();
 		String date = mDateEdit.getText().toString();
 		String comment = mCommentEdit.getText().toString();
-		
+
 		mEvaluatorEdit.setVisibility(View.GONE);
 		mDateEdit.setVisibility(View.GONE);
 		mCommentEdit.setVisibility(View.GONE);
-		
+
 		mData.setEvaluator(evaluator);
 		mData.setDate(date);
 		mData.setComment(comment);
-		
+
 		mEvaluator.setText(evaluator);
 		mDate.setText(date);
 		mComment.setText(comment);
-		
+
 		mDataList.getSubData().set(mProjectNumber, mData);
-		
+
 		mDB.updateCL(mDataList);
-		
+
 	}
-	
-	
+
+	@Override
+	public void onInfo() {
+		super.onInfo();
+		final Dialog dialog = new Dialog(CLDetailActivity.this);
+		dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		dialog.setContentView(R.layout.dialog_cl_manual);
+
+		ListView list = (ListView) dialog.findViewById(R.id.dialog_cl_list);
+		list.setAdapter(new CLManualDialogAdapter(this));
+
+		Button gotit = (Button) dialog.findViewById(R.id.dialog_cl_gotit);
+		gotit.setOnClickListener(new Button.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				dialog.dismiss();
+			}
+
+		});
+
+		dialog.show();
+	}
 
 }
